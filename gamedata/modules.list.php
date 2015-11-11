69169fd8eef1b2d13cbca59f58607750c5c0eb0b
lostgirlarisu <524853923@qq.com>
2015-11-11 19:27:41 +0800

    完成技能【言灵】

--- gamedata/modules.list.php
+++ gamedata/modules.list.php
@@ -155,6 +155,7 @@ skill408,extra/club/skills/skill408/,1
 skill409,extra/club/skills/skill409/,1
 skill2003,extra/club/skills/skill2003/,1
 skill2004,extra/club/skills/skill2004/,1
+skill2006,extra/club/skills/skill2006/,1
 skill2005,extra/club/skills/skill2005/,1
 skill53,extra/club/skills/skill53/,1
 skill55,extra/club/skills/skill55/,1
@@ -238,4 +239,3 @@ skill27,extra/club/skills/skill27/,1
 skill43,extra/club/skills/skill43/,1
 skill56,extra/club/skills/skill56/,1
 skill57,extra/club/skills/skill57/,1
-skill2006,extra/club/skills/skill2006/,0
